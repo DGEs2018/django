355cf305e4479aacfac70cc0b21445adb8b81fab
Dawit <dawit@ginetta.net>
2020-08-14 13:54:22 +0200

    Define detail, results and vote functions

--- polls1/views.py
+++ polls1/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Hello user, welcome to the polls index.')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response ("You're looking at the result of question %s.")
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
